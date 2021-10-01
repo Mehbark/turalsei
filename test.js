555c3bc400c771597e614b440af6880191a127c0
Mehbark <clarkemailtest@gmail.com>
2021-09-30 21:20:49 -0400

    main display stuff, options next

--- test.js
+++ test.js
@@ -1,2 +1,10 @@
-import { genUrl } from "./turalsei.js";
+import { genUrl, brain } from "./turalsei.js";
 console.log(genUrl("test1\ntest2") + "\n");
+
+let testBrain = new brain("", "");
+
+console.log(testBrain.pointerLine());
+testBrain.dataPointer++;
+console.log(testBrain.pointerLine());
+
+console.log(testBrain.finalUrl());
--- turalsei.js
+++ turalsei.js
@@ -14,35 +14,66 @@ export function genUrl(
 export class brain {
   constructor(code, input = false, options = {}) {
     this.code = code;
-
-    if (delay < 1000) {
-      delay = 1000;
-    }
-    this.delay = delay;
+    this.input = input;
 
     let defaultOptions = {
       delay: 1000,
-      pointerChar: "^",
+      pointerChar: "^^",
       dataSize: 256,
+      maxNumber: 256,
+      numberWrap: true,
       dataPointerWrap: true,
+      maxCharsPerLine: 25,
       customUrl:
         "https://www.demirramon.com/gen/undertale_text_box.png?text=$$$$&box=deltarune&boxcolor=ffffff&character=deltarune-ralsei&expression=angry&charcolor=colored&font=determination&asterisk=ffffff&mode=darkworld",
       instant: false,
+      outputOnly: false,
     };
     this.parseOptions(defaultOptions, options);
 
-    this.input = input;
+    this.delay = this.delay > 1000 ? this.delay : 1000;
 
     this.dataPointer = 0;
+    this.data = "1".repeat(this.dataSize).split("");
+    this.data = this.data.map((x) => parseInt(x));
+
+    this.intructionPointer = 0;
+
+    this.output = "";
+  }
+
+  memoryLine() {
+    return this.data
+      .map((x) => {
+        let p = parseInt(x.toString(16), 16).toString();
+        return p.length > 1 ? "" : "0" + p;
+      })
+      .join("");
+  }
+
+  pointerLine() {
+    return " ".repeat(this.dataPointer * 2) + this.pointerChar.slice(0, 2);
+  }
+
+  finalText() {
+    return [
+      this.memoryLine().slice(0, this.maxCharsPerLine),
+      this.pointerLine().slice(0, this.maxCharsPerLine),
+      this.output.slice(0, this.maxCharsPerLine),
+    ].join("\n");
+  }
+
+  finalUrl() {
+    return genUrl(this.finalText(), this.customUrl);
   }
 
   parseOptions(defaultOptions, options) {
-    for (let opt in defaultOptions.keys) {
+    Object.keys(defaultOptions).forEach((opt) => {
       if (options[opt] !== undefined) {
         this[opt] = options[opt];
       } else {
         this[opt] = defaultOptions[opt];
       }
-    }
+    });
   }
 }
